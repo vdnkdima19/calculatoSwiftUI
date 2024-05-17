ead570f007021caafe4bd7e3776e79431a4a691a
vdnk_d <dmitryvdvnk@gmail.com>
2024-05-17 16:34:53 +0300

    Initial Commit

---
+++ calculatorSwiftUI/calculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorSwiftUIApp.swift
+//  calculatorSwiftUI
+//
+//  Created by Dmitry Vdovenko on 17.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct calculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
